8d4647e473a4ddc92cecd74811c14a98c656ea9a
Robert  Henrique <roberthenrique2011@gmail.com>
2017-09-25 21:56:15 -0400

    Binding de Text e SwitchCell

--- TestDrive/TestDrive/Views/DetalheView.xaml.cs
+++ TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -8,16 +8,44 @@ using Xamarin.Forms;
 
 namespace TestDrive.Views
 {
-	public partial class DetalheView : ContentPage
-	{
+    public partial class DetalheView : ContentPage
+    {
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
         public Veiculo Veiculo { get; set; }
 
-        public DetalheView (Veiculo veiculo)
-		{
-			InitializeComponent ();
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", FREIO_ABS);
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO);
+            }
+        }
+
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", MP3_PLAYER);
+            }
+        }
+
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();
             this.Veiculo = veiculo;
             this.BindingContext = this;
-		}
+        }
 
         private void buttonProximo_Clicked(object sender, EventArgs e)
         {
