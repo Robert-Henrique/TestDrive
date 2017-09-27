da8b0173e0f41679a45c7ae0a1cd6d588e0284bd
Robert  Henrique <roberthenrique2011@gmail.com>
2017-09-26 22:05:44 -0400

    Binding para DatePicker e TimePicker

--- TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -9,18 +9,33 @@ using Xamarin.Forms.Xaml;
 
 namespace TestDrive.Views
 {
-	public partial class AgendamentoView : ContentPage
-	{
+    public partial class AgendamentoView : ContentPage
+    {
         public Veiculo Veiculo { get; set; }
         public string Nome { get; set; }
         public string Fone { get; set; }
         public string Email { get; set; }
 
-        public AgendamentoView (Veiculo veiculo)
-		{
-			InitializeComponent ();
+        DateTime dataAgendamento = DateTime.Now;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+
+        public TimeSpan HoraAgendamento { get; set; }
+
+        public AgendamentoView(Veiculo veiculo)
+        {
+            InitializeComponent();
             this.Veiculo = veiculo;
             this.BindingContext = this;
-		}
-	}
+        }
+    }
 }
