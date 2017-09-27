78075c8da1cb3a0b279eaa639d181b2bb1f21bce
Robert  Henrique <roberthenrique2011@gmail.com>
2017-09-26 21:59:45 -0400

    DatePicker, TimePicker e ViewCell

--- TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -12,6 +12,9 @@ namespace TestDrive.Views
 	public partial class AgendamentoView : ContentPage
 	{
         public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
 
         public AgendamentoView (Veiculo veiculo)
 		{
