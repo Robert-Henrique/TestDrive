fa3480bd80c2bfb3aa6433381ab93a3cc901e144
Robert  Henrique <roberthenrique2011@gmail.com>
2017-10-03 21:01:24 -0400

    Tratando erro de comunicação com o servidor

--- TestDrive/TestDrive/LoginService.cs
+++ TestDrive/TestDrive/LoginService.cs
@@ -12,22 +12,30 @@ namespace TestDrive
     {
         public async Task FazerLogin(Login login)
         {
-            using (var cliente = new HttpClient())
+            try
             {
-                var camposFormulario = new FormUrlEncodedContent(new[]
+                using (var cliente = new HttpClient())
                 {
-                        new KeyValuePair<string, string>("email", login.email),
-                        new KeyValuePair<string, string>("senha", login.senha)
-                    });
+                    var camposFormulario = new FormUrlEncodedContent(new[]
+                    {
+                    new KeyValuePair<string, string>("email", login.email),
+                    new KeyValuePair<string, string>("senha", login.senha)
+                });
 
-                cliente.BaseAddress = new Uri("https://aluracar.herokuapp.com");
-                var resultado = await cliente.PostAsync("/login", camposFormulario);
+                    cliente.BaseAddress = new Uri("https://aluracar.herokuapp.com");
+                    var resultado = await cliente.PostAsync("/login", camposFormulario);
 
-                if (resultado.IsSuccessStatusCode)
-                    MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
-                else
-                    MessagingCenter.Send<LoginException>(new LoginException("Usuário ou senha incorretos!"), "FalhaLogin");
-            };
+                    if (resultado.IsSuccessStatusCode)
+                        MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
+                    else
+                        MessagingCenter.Send<LoginException>(new LoginException("Usuário ou senha incorretos!"), "FalhaLogin");
+                };
+            }
+            catch
+            {
+                MessagingCenter.Send<LoginException>(new LoginException("Occoreu um erro de comunicação com o servidor! " + 
+                                                                        "Por favor verifique sua conexão e tente novamente mais tarde"), "FalhaLogin");
+            }
         }
     }
 
