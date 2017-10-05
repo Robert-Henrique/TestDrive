42db981ede64471890ffec5596afc5f8d3714c5b
Robert  Henrique <roberthenrique2011@gmail.com>
2017-10-04 22:27:31 -0400

    Intent (Parte IV - Aula 06)

--- TestDrive/TestDrive.Android/MainActivity.cs
+++ TestDrive/TestDrive.Android/MainActivity.cs
@@ -20,12 +20,29 @@ namespace TestDrive.Droid
         global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity,
         ICamera
     {
+        static Java.IO.File arquivoImagem;
+
         public void TirarFoto()
         {
             var intent = new Intent(MediaStore.ActionImageCapture);
+            arquivoImagem = PegarArquivoImagem();
+
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(arquivoImagem));
 
             var activity = Forms.Context as Activity;
             activity.StartActivityForResult(intent, 0);
+        }
+
+        private static Java.IO.File PegarArquivoImagem()
+        {
+            Java.IO.File arquivoImagem;
+            Java.IO.File diretorio = new Java.IO.File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures), "Imagens");
+
+            if (!diretorio.Exists())
+                diretorio.Mkdirs();
+
+            arquivoImagem = new Java.IO.File(diretorio, "MinhaFoto.jpg");
+            return arquivoImagem;
         }
 
         protected override void OnCreate(Bundle bundle)
@@ -37,6 +54,13 @@ namespace TestDrive.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new TestDrive.App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            MessagingCenter.Send<Java.IO.File>(arquivoImagem, "TirarFoto");
         }
     }
 }
