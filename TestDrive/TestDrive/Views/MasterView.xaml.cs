8e16c1a322d7a07ca7be8f3bee02672f17acaeb6
Robert  Henrique <roberthenrique2011@gmail.com>
2017-10-04 20:40:43 -0400

    EditarPerfilCommand

--- TestDrive/TestDrive/Views/MasterView.xaml.cs
+++ TestDrive/TestDrive/Views/MasterView.xaml.cs
@@ -10,13 +10,31 @@ using Xamarin.Forms.Xaml;
 
 namespace TestDrive.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MasterView : TabbedPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterView : TabbedPage
+    {
         public MasterView(Usuario usuario)
         {
             InitializeComponent();
             this.BindingContext = new MasterViewModel(usuario);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil",
+                (usuario) =>
+                {
+                    this.CurrentPage = this.Children[1];
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            MessagingCenter.Unsubscribe<Usuario>(this, "EditarPerfil");
         }
     }
 }
--- TestDrive/TestDrive/ViewsModels/MasterViewModel.cs
+++ TestDrive/TestDrive/ViewsModels/MasterViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using TestDrive.Models;
+using Xamarin.Forms;
 
 namespace TestDrive.ViewsModels
 {
     public class MasterViewModel
     {
-       
+
         public string Nome
         {
             get { return this.usuario.nome; }
@@ -24,9 +26,16 @@ namespace TestDrive.ViewsModels
 
         private readonly Usuario usuario;
 
+        public ICommand EditarPerfilCommand { get; private set; }
+
         public MasterViewModel(Usuario usuario)
         {
             this.usuario = usuario;
+
+            EditarPerfilCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(usuario, "EditarPerfil");
+            });
         }
     }
 }
